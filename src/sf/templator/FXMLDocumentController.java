7af1477e676e85f3741be5d3d1470c4c67f3727c
Igors Krelnins <ikreknin@gmail.com>
2017-06-14 21:22:03 +0300

    TreeView & HTML Editor added

--- src/sf/templator/FXMLDocumentController.java
+++ src/sf/templator/FXMLDocumentController.java
@@ -17,7 +17,6 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
     }
 
     @Override
